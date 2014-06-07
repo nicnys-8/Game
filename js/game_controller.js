1ab37866fff5b7ee0749919e818785685d6b4935
nicnys-8 <nirivonsiri@gmail.com>
2014-06-07 11:06:22 +0200

    Created a game controller file
    Very necessary commit, this…

---
+++ js/game_controller.js
@@ -0,0 +1,14 @@
+/**
+Returns a game controller object
+*/
+function GameController() {
+
+	//================================
+	// Private functions and variables
+	//================================
+
+
+	//=================
+	// Public Interface
+	//=================
+}
