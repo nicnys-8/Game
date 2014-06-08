1c26d1240986c9cfb9f2b98286833a2b3355fabb
nicnys-8 <nirivonsiri@gmail.com>
2014-06-08 14:06:23 +0200

    Important comments

--- js/levels.js
+++ js/levels.js
@@ -18,5 +18,5 @@ var Levels = {
 men än så länge följer inte grafiken med då man sätter width på objekt
 */
 for (var i = 0; i < 12; i++) {
-	Levels.level1["objects"].push({name: "block", x: 16*i, y: 128, width: 16, height :16});
+	Levels.level1["objects"].push({name: "block", x: 16*i, y: 128, width: 16, height :16}); // Sergeant kolonplacering slår till igen
 }
