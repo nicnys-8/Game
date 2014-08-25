e304976204ff7ef8c63691e570222b6ca4565b01
jimnys-8 <snaggadhagga@gmail.com>
2014-08-26 01:10:14 +0200

    New function for creating object määby

--- js/objects/object_factory.js
+++ js/objects/object_factory.js
@@ -5,4 +5,19 @@ Alla spelobject sätts fast på den här, t.ex. ObjectFactory.Moving;
 det gör att man kan parsa banor lite snyggare i gamestate,
 då alla objekttyper kan accessas som properties på det här objektet
 */
-var ObjectFactory = ObjectFactory ||  {};
+var ObjectFactory = ObjectFactory ||  {
+    
+    // Nåt sånt här kan man också ha, men då måste vi skriva om
+    // konstruktÖrerna så att de tar alla parametrar i ett objekt.
+    // Alternativt {name:"Nånting", params:{...}}
+    createObject : function(description) {
+        var constr = this[description.name],
+            object = null;
+        if (constr) {
+            object = new constr(description);
+        } else {
+            console.warn("Trying to create unknown object " + description.name);
+        }
+        return object;
+    }
+};
