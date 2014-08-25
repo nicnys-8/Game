7c618b06dea4974e2f8afa302e32a98c68a12a30
jimnys-8 <snaggadhagga@gmail.com>
2014-08-26 01:09:37 +0200

    Changed 'behavior' to object instead of array
    More efficient lookups!

--- js/objects/game_object.js
+++ js/objects/game_object.js
@@ -7,7 +7,7 @@ GameObject = function() {
 	// Instance variables
 	//===================
 
-	this.behaviors = [];
+	this.behaviors = {}; // [];
 	this.ticks = [];
 	this.uid; // This is set in GameState, when the level is parsed
 };
@@ -20,33 +20,43 @@ GameObject = function() {
 Returns true if the object has the given behavior, false otherwise
 */
 GameObject.prototype.hasBehavior = function(behavior) {
+    return !!this.behaviors[behavior];
+    /*
 	for (var i = 0; i < this.behaviors.length; i++) {
 		if (behavior === this.behaviors[i]) {
 			return true;
 		}
 	}
 	return false;
+     */
 };
 
 /**
 Adds a behavior to the sprite object
 */
 GameObject.prototype.addBehavior = function(behavior) {
-	var properties = behavior.getProperties();
+    
+    if (typeof behavior === "function") {
+        // Subclasses can add custom behavior with functions
+        this.ticks.push(behavior);
+        return;
+    }
+    
+    var properties = behavior.getProperties();
 
-		// Add all behavior properties
-		for (var p in properties) {
-			this[p] = properties[p];
-		}
+    // Add all behavior properties
+    for (var p in properties) {
+        this[p] = properties[p];
+    }
 
-		// Add the name of the behavior
-		this.behaviors.push(behavior.name);
-		
-		// Modify the target's tick function
-		if (behavior.tick) {
-			this.ticks.push(behavior.tick);
-		}
-	};
+    // Add the name of the behavior
+    this.behaviors[behavior.name] = true; // .push(behavior.name);
+    
+    // Modify the target's tick function
+    if (behavior.tick) {
+        this.ticks.push(behavior.tick);
+    }
+};
 
 /**
 Actions to perform at each iteration of the game loop
